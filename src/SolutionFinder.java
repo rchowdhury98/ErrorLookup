8ce03cef813df9dd45806fe947e96358c9339259
pierfier <fabrispierre@gmail.com>
2017-12-02 17:20:37 -0500

    Working on setting up socket request

--- src/SolutionFinder.java
+++ src/SolutionFinder.java
@@ -54,15 +54,54 @@ public class LinkQuery{
     public LinkQuery(){
         httpRequest = "";
         url = "https://www.googleapis.com/customsearch/v1?key=";
+        socket = new Socket("www.google.com", 80);
     }
     
-    //Grab API key from file. Use file readers
-    void readKey(String file){
+    //Grab API key from file and add it to the url
+    private void appendKeyFromFile(String file){
         key = "";
+        
+        //INSERT CODE for reading in file
+
+        url += key;
+    }
+
+    //Grab custom search engine information from file
+    private void appendCSEFromFile(String file){
+        cx = "";
+        
+        //INSERT CODE for reading in file
+
+        url += cx; 
+    }
+
+    //Example from website:
+    //GET https://www.googleapis.com/customsearch/v1?key=INSERT_YOUR_API_KEY&cx=017576662512468239146:omuauf_lfve&q=lectures
+
+    
+    //Create the http request from the search element of interest, the file with the API key, and the file that contains the name of the custom search engine
+    public void composeHTTPRequest(String searchElement, String keyFile, String cseFile){
+        //Compose the URL with the search element,
+        //key file and csefile
+        //the append methods will create filereading objects to
+        //grab the respective values
+        appendKeyFromFile(keyFile);
+        url += "&cx=";
+        appendCSEFromFile(cseFile);
+        url += "&q=" + searchElement;
+        
+        //Actual composition of the HTTPRequest header
+
+        httpRequest = "GET " + url + " HTTP/1.1\r\n";
+        httpRequest += "Content-Type: application/json\r\n";
+        httpRequest += "\r\n\r\n";
 
     }
 
-    void composeUrl(String searchElement){
-        url += searchElement;
+    //write the request to the socket
+    public void sendRequest(){
+     /*   try{
+        }catch(Exception e){e.printStrackTrace();}
+    */
     }
 }
